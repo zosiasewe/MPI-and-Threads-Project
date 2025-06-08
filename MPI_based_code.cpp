1c33c1714447dbfc32e215cfdafa90b00d79937a
Zofia Sewerynska <zosiasewe2@gmail.com>
2025-06-08 15:00:46 +0200

    Add files via upload

---
+++ MPI_based_code.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+#include "mpi.h"
+using namespace std;
+
+const int ARRSIZE = 100000;
+
+class MPI_Producer {
+    int value = 0;
+    int array_num;
+    int world_rank;
+    int world_size;
+
+public:
+    MPI_Producer(int n, int rank, int size) : array_num(n), world_rank(rank), world_size(size) {}
+
+    void run() {
+        array<int, ARRSIZE> message;
+        double start_time = MPI_Wtime();
+
+        for (int i = 0; i < array_num; ++i) {
+            for (auto& element : message) {
+                element = value++;
+            }
+
+            int consumer_rank = 1 + (i % (world_size - 1));
+            MPI_Send(message.data(), ARRSIZE, MPI_INT, consumer_rank, 0, MPI_COMM_WORLD);
+        }
+
+        array<int, ARRSIZE> done_message;
+        done_message.fill(-1);
+        for (int rank = 1; rank < world_size; ++rank) {
+            MPI_Send(done_message.data(), ARRSIZE, MPI_INT, rank, 0, MPI_COMM_WORLD);
+        }
+
+        double end_time = MPI_Wtime();
+    }
+};
+
+class MPI_Consumer {
+    int world_rank;
+    int local_count = 0;
+    double local_time_ms = 0;
+
+public:
+    MPI_Consumer(int rank) : world_rank(rank) {}
+
+    void run() {
+        array<int, ARRSIZE> message;
+        double start = MPI_Wtime();
+
+        while (true) {
+            MPI_Status status;
+            MPI_Recv(message.data(), ARRSIZE, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+
+            if (message[0] == -1) break;
+
+            sort(message.begin(), message.end());
+            ++local_count;
+        }
+
+        double end = MPI_Wtime();
+        local_time_ms = (end - start) * 1000.0;
+    }
+
+    int getCount() const { return local_count; }
+    double getTimeMs() const { return local_time_ms; }
+};
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    const int array_num = 1000;
+    const int queue_capacity = 300;
+
+    double global_start = MPI_Wtime();
+
+    int local_count = 0;
+    double local_time = 0;
+
+    if (world_rank == 0) {
+        MPI_Producer producer(array_num, world_rank, world_size);
+        producer.run();
+        local_time = MPI_Wtime() - global_start;
+    }
+    else {
+        MPI_Consumer consumer(world_rank);
+        consumer.run();
+        local_count = consumer.getCount();
+        local_time = consumer.getTimeMs() / 1000.0;  // seconds
+    }
+
+    vector<int> all_counts(world_size, 0);
+    vector<double> all_times(world_size, 0.0);
+
+    MPI_Gather(&local_count, 1, MPI_INT, all_counts.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Gather(&local_time, 1, MPI_DOUBLE, all_times.data(), 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    double max_runtime = 0.0;
+    MPI_Reduce(&local_time, &max_runtime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (world_rank == 0) {
+        cout << "queue capacity: " << queue_capacity << "\n";
+        cout << "number of arrays to produce: " << array_num << "\n";
+        cout << "number of consumer threads: " << world_size - 1 << "\n";
+
+        for (int i = 1; i < world_size; ++i) {
+            cout << "Process " << i << " processed " << all_counts[i]
+                << " arrays in " << static_cast<int>(all_times[i] * 1000) << " ms\n";
+        }
+        cout << "Total runtime: " << static_cast<int>(max_runtime * 1000) << " ms\n";
+    }
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ Thread_based_code.cpp
@@ -0,0 +1,169 @@
+﻿#include <iostream>
+#include <array>
+#include <queue>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <condition_variable>
+#include <algorithm>
+#include <numeric>
+#include <chrono>
+
+using namespace std;
+
+const int ARRSIZE = 100000;
+mutex print_mutex;
+
+class Queue {
+    queue<array<int, ARRSIZE>> q;
+    size_t max_size;
+    mutex mtx;
+    condition_variable cv; // for blocking threads until queue state changes
+    bool done_producing = false;
+
+public:
+    Queue(size_t max_length) : max_size(max_length) {}
+    void add(const array<int, ARRSIZE>& arr) {
+        unique_lock<mutex> lock(mtx);
+        cv.wait(lock, [this]() { return q.size() < max_size; });
+        q.push(arr);
+        cv.notify_all();
+    }
+
+    bool remove(array<int, ARRSIZE>& result) {
+        unique_lock<mutex> lock(mtx);
+        cv.wait(lock, [this]() { return !q.empty() || done_producing; });
+
+        if (q.empty() && done_producing) {
+            return false;
+        }
+
+        result = q.front();
+        q.pop();
+        cv.notify_all();
+        return true;
+    }
+
+    void done() {
+        unique_lock<mutex> lock(mtx);
+        done_producing = true;
+        cv.notify_all();
+    }
+};
+
+class Producer {
+    Queue& queue;
+    int array_num;
+
+public:
+    Producer(Queue& q, int n) : queue(q), array_num(n) {}
+    void run() {
+        {
+            lock_guard<mutex> lock(print_mutex);
+            cout << "Start - Producer\n";
+        }
+
+        auto start = chrono::high_resolution_clock::now();
+
+        for (int i = 0; i < array_num; ++i) {
+            array<int, ARRSIZE> arr;
+            int value = 0;
+            for (auto it = arr.begin(); it != arr.end(); ++it) {
+                *it = value++;
+            }
+            queue.add(arr);
+        }
+
+        queue.done();
+
+        auto end = chrono::high_resolution_clock::now();
+        {
+            lock_guard<mutex> lock(print_mutex);
+            cout << "Stop - Producer - " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms\n";
+        }
+    }
+};
+
+class Consumer {
+    Queue& queue;
+    int consumed = 0;
+
+    int calculateChecksum(const array<int, ARRSIZE>& arr) {
+        return accumulate(arr.begin(), arr.end(), 0);
+    }
+
+public:
+    Consumer(Queue& q) : queue(q) {}
+    void run() {
+        {
+            lock_guard<mutex> lock(print_mutex);
+            cout << "Thread " << this_thread::get_id() << " - Start\n";
+        }
+        auto start = chrono::high_resolution_clock::now();
+
+        while (true) {
+            array<int, ARRSIZE> arr;
+            bool success = queue.remove(arr);
+            if (!success) break;
+
+            sort(arr.begin(), arr.end());
+            int checksum = calculateChecksum(arr);
+
+            {
+                lock_guard<mutex> lock(print_mutex);
+                cout << "Thread " << this_thread::get_id() << " sum: " << checksum << "\n";
+            }
+
+            ++consumed;
+        }
+        auto end = chrono::high_resolution_clock::now();
+
+        {
+            lock_guard<mutex> lock(print_mutex);
+            cout << "Thread " << this_thread::get_id() << " processed " << consumed
+                << " arrays in " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms\n";
+        }
+    }
+};
+
+int main() {
+    const size_t queue_capacity = 300;
+    const int array_num = 1000;
+    const int consumer_num = 64;
+
+    {
+        lock_guard<mutex> lock(print_mutex);
+        cout << "hardware concurrency: " << thread::hardware_concurrency() << " threads\n";
+        cout << "queue capacity: " << queue_capacity << "\n";
+        cout << "number of arrays to produce: " << array_num << "\n";
+        cout << "number of consumer threads: " << consumer_num << "\n";
+    }
+
+    auto global_start = chrono::high_resolution_clock::now();
+
+    Queue queue(queue_capacity);
+    Producer producer(queue, array_num);
+
+    thread producerThread(&Producer::run, &producer);
+    vector<thread> consumer_threads;
+    vector<unique_ptr<Consumer>> consumers;
+
+    for (int i = 0; i < consumer_num; ++i) {
+        consumers.emplace_back(make_unique<Consumer>(queue));
+        consumer_threads.emplace_back(&Consumer::run, consumers.back().get());
+    }
+
+    producerThread.join();
+    for (auto& t : consumer_threads) {
+        t.join();
+    }
+    auto global_end = chrono::high_resolution_clock::now();
+
+    {
+        lock_guard<mutex> lock(print_mutex);
+        cout << "End\n";
+        cout << "Total runtime: " << chrono::duration_cast<chrono::milliseconds>(global_end - global_start).count() << " ms\n";
+    }
+
+    return 0;
+}
